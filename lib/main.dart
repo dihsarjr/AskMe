cc8de2f8b427bdfcbc73e0c92a6cc7d3ef3b0826
Mohammed Rashid k <mohammed1rashid786@gmail.com>
2020-06-12 12:07:19 +0530

    ImageLoaded

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,21 @@ class ballPage extends StatelessWidget {
         title: Text('Ask Me Anything'),
         backgroundColor: Colors.black,
       ),
-      body: Container(),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("images/ball1.png"),
     );
   }
 }
